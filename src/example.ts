68c0ec05dddfdbd387fa25c88c7c249b0fb747ff
Serban Blebea <slabestecuserban@gmail.com>
2019-06-02 22:59:10 +0100

    remove example file

--- src/example.ts
+++
@@ -1,9 +0,0 @@
-import { Director, Car, CarBuilder } from './Creational'
-
-let director = new Director<Car>(new CarBuilder())
-
-let cheapCar     = director.buildCheapCar('Dacia', 'Logan', 5, 4, 'diesel')
-let expensiveCar = director.buildExpensiveCar('Mercedes', 'A342', 5, 4, 'petrol')
-
-console.log(cheapCar)
-console.log(expensiveCar)
